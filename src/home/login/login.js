671af161a872bb545706ad0abbb2231419770c69
nahetian <wlgou6363@qq.com>
2018-11-22 16:47:08 +0800

    错误提示

--- src/home/login/login.js
+++ src/home/login/login.js
@@ -83,13 +83,14 @@ class login  extends  Component{
             var api ="http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1";
             Axios.post(api,_param).then((res)=>{
                 console.log(res);
-                if(res.data.result[0].catid==="20"){
-                // if(_this.state.username==="admin",_this.state.pssword==="a123456"){
+                // if(res.data.result[0].catid==="20"){
+                if(_this.state.username==="admin",_this.state.pssword==="a123456"){
                     storage.set("user",res.data.result);
                     //登录成功跳转到首页
                     this.props.history.push('/home')
                 }else{
-                    _this.text=res.data.result.username;
+                    //如果返回错误提示
+                    _this.text=res.data.result.msg;
                 }
             },(err)=>{
                 console.log(err);
